7db44184c1c5615f893249fc7ea90ce337de1b8b
Lucas <lucasfmartins16@usp.br>
2020-09-07 16:42:49 -0300

    Message

--- a/tabuleiro.h
+++
@@ -1,9 +0,0 @@
-typedef  struct Peca{
-    char forma;
-    char cor;
-} Peca;
-
-Peca** aloca_tabuleiro(int m, int n);
-void cores(char c, char d);
-void print_tabuleiro(Peca **matriz, int m, int n);
-Peca** update_tabuleiro(Peca **matriz, int *n_linhas, int *n_colunas, int **track, int pos);
